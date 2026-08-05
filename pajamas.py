0041fbf57150fed313f4d015559a7460e78818a2
Erik Xavier Acevedo Silva <exavier.as07@gmail.com>
2026-08-04 23:21:25 -0500

    inventory: products v2 (segment + size-by-segment)

--- pajamas.py
+++ pajamas.py
@@ -1,14 +1,20 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-size_a = (4, 6, 8, 10, 12, 14, 16)
+size_a = [4, 6, 8, 10, 12, 14, 16]
 size_b = ['XS', 'X', 'M', 'L', 'XL']
+
+size_by_segment = {
+    'kids' : size_a,     
+    'women' : size_b,  
+    'men': size_b,      
+}
 
 rng = np.random.default_rng()
 #index= rng.integers(low=100, high=999, size= 250)
-segmento = rng.choice(size_a, 250)
+segment = rng.choice(['women', 'kids', 'men'], 250)
 uni = rng.integers(8000, 30000, size = 250)
 re = rng.integers(50, 100, size = 250)
 ini = rng.integers(100, 200, size = 250)
@@ -16,8 +22,14 @@ material = rng.choice(['acetate', 'cotton'], 250)
 ty= rng.choice(['short sleeved', 'long sleeved', 'no sleeves'], 250)
 printed = rng.choice(['Yes', 'No'], 250)
 origin = rng.choice(['Own', 'Resell'], 250)
+
+size = []
+for seg in segment:
+    size.append(rng.choice(size_by_segment[seg]))
+
 pajamas_k = {
-    'Size' : pd.Series(segmento),
+    'Segment' : pd.Series(segment),
+    'Size' : pd.Series(size),
     'Material' : pd.Series(material),
     'Type' : pd.Series(ty),
     'Print': pd.Series(printed),
@@ -27,16 +39,6 @@ pajamas_k = {
     'Initial Stock': pd.Series(ini)
 }
 
-pajamas_s = {
-    'size' : size_b,
-    'material' : ('acetate', 'cotton'),
-    'type' : ('short sleeved', 'long sleeved', 'no sleeves'),
-    'print': ['Yes', 'No'],
-    'origin' : ['Own', 'Resell']
-}
-
-
 df = pd.DataFrame(pajamas_k)
-#df1 = pd.concat([df, price_k, reorder_k, stock_k], axis=1)
 
 print(df)
